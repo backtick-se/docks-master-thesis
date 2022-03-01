2ecb57db8a30e468c69d0787582f352a3c78e6e1
Szymon Stypa <shabo.o@hotmail.com>
2022-03-01 15:01:35 +0100

    staticmethod

--- temp.py
+++ temp.py
@@ -7,3 +7,13 @@ class Visitor(ast.NodeVisitor):
 	def visit_Name(self, node):
 		self.names.append(node)
 		self.generic_visit(node)
+
+	@staticmethod
+	def get_names(code: str):
+		if code is None: return []
+
+		tree = ast.parse(code)
+		visi = Visitor()
+		visi.visit(tree)
+
+		return [*map(lambda nn: nn.id, visi.names)]
