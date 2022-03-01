44a085dd8cd7a21ce6ab205c2c1a36d32341ab90
Szymon Stypa <shabo.o@hotmail.com>
2022-03-01 16:13:38 +0100

    new name extraction test

--- temp.py
+++ temp.py
@@ -3,10 +3,14 @@ import ast
 class Visitor(ast.NodeVisitor):
 	def __init__(self):
 		self.names = []
-	
-	def visit_Name(self, node):
-		self.names.append(node)
-		self.generic_visit(node)
+
+	def generic_visit(self, node):
+		try:
+			self.names.append(node.name)
+		except:
+			pass
+		
+		return super().generic_visit(node)
 
 	@staticmethod
 	def get_names(code: str):
