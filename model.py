228a13349fc44e43ab0c4e933c2264aaa64839dd
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2022-03-03 11:37:39 +0100

    Evaluation script

---
+++ model.py
@@ -0,0 +1,5 @@
+
+# Predict if a given commit should be flagged
+# The evaluation script has already checked out this commit before calling this function.
+def predict(eval_wd, commit):
+    return True
