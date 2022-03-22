45c9212cee1439abbf70614a96d6224a391253e2
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2022-03-22 08:59:59 +0100

    Gathered PR from cowait

--- github_fetch.py
+++ github_fetch.py
@@ -24,6 +24,15 @@ def parse_pull_requests(responses):
 
     return pull_requests
 
+def parse_requests(input_file, output_file):
+    with open(input_file, 'rb') as f:
+        data = pickle.load(f)
+    assert set([r.status_code for r in data]) == set([200])
+    pr = parse_pull_requests(data)
+    print(f"Found {len(pr)} prs")
+    with open(output_file, 'wb') as f:
+        pickle.dump(pr, f, protocol=pickle.HIGHEST_PROTOCOL)
+
 @click.command()
 @click.option('--token', '-t', help='GitHub API user authentication token')
 @click.option('--url', '-u', help='URL to start fetching')
@@ -31,16 +40,12 @@ def parse_pull_requests(responses):
 def run(token: str, url: str, out: str):
     """
     Ex. usage:
-    python3 github_fetch.py -t $token \-u "https://api.github.com/repos/pandas-dev/pandas/issues?per_page=100&state=open" -o issues.pickles
+    python3 github_fetch.py -t $token -u "https://api.github.com/repos/pandas-dev/pandas/issues?per_page=100&state=open" -o issues.pickle
 
     python3
     import pickle
-    from github_fetch import parse_pull_requests
-    f = open('data/issues/issues.pickles', 'rb')
-    data = pickle.load(f)
-    set([r.status_code for r in data]) # Should be {200}
-    pr = parse_pull_requests(data)
-    len(pr)
+    from github_fetch import *
+    parse_requests('issues.pickle', 'pr.pickle')
     """
     responses = fetch_responses(token, url)
     print(len(responses), "responses", responses[:3])
