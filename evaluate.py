f90ca832378dc6274a46306d62e25140e7a04fca
Szymon Stypa <shabo.o@hotmail.com>
2022-04-07 11:19:54 +0200

    some utils created to easily evaluate and predict with fine-tuned model

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,61 @@
 from utils import categories
 from sklearn.metrics import confusion_matrix, classification_report
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import matplotlib.pyplot as plt
+import torch
 
 def eval(y_true, y_pred):
     print(classification_report(y_true, y_pred, labels=categories))
     print(confusion_matrix(y_true, y_pred, labels=categories))
+
+def load_trained(base, path):
+	model = AutoModelForSequenceClassification.from_pretrained(base, num_labels=len(categories))
+	tokenizer = AutoTokenizer.from_pretrained(base)
+
+	cp = torch.load(path, map_location=torch.device('cpu'))
+	model.load_state_dict(cp['model_state_dict'])
+	metrics = cp['metrics']
+
+	model.eval()
+
+	def predict(text):
+		inputs = tokenizer(text, padding='max_length', truncation=True, return_tensors="pt")
+		outputs = model(**inputs)
+		logits = outputs.logits
+		pred = torch.argmax(logits, dim=-1)
+		return categories[pred]
+
+	def plot_progress():
+		taccs = [*map(lambda e: e['train_acc']['accuracy'], metrics)]
+		vaccs = [*map(lambda e: e['val_acc']['accuracy'], metrics)]
+		tloss = [*map(lambda e: e['train_loss'].item(), metrics)]
+		vloss = [*map(lambda e: e['val_loss'].item(), metrics)]
+		tfone = [*map(lambda e: e['train_f1']['f1'], metrics)]
+		vfone = [*map(lambda e: e['val_f1']['f1'], metrics)]
+
+		fig, ax = plt.subplots(3, sharex=True)
+		fig.suptitle('Model Training Progress')
+
+		x = range(1, len(metrics) + 1)
+
+		fig.set_figheight(8)
+		fig.set_figwidth(10)
+
+		ax[0].plot(x, taccs, 'bo-', label='Training')
+		ax[0].plot(x, vaccs, 'go-', label='Validation')
+		ax[0].set_ylabel('Accuracy')
+
+		ax[1].plot(x, tloss, 'bo-')
+		ax[1].plot(x, vloss, 'go-')
+		ax[1].set_ylabel('Loss')
+
+		ax[2].plot(x, tfone, 'bo-')
+		ax[2].plot(x, vfone, 'go-')
+		ax[2].set_ylabel('Macro F1')
+
+		fig.legend()
+		plt.xlabel('Epoch')
+		plt.xticks(x)
+		plt.show()
+	
+	return predict, plot_progress
