1a5632b5bf7a3501f4b1145d14bc11931dbb2032
Szymon Stypa <shabo.o@hotmail.com>
2022-04-11 16:30:51 +0200

    fix

--- evaluate.py
+++ evaluate.py
@@ -51,7 +51,7 @@ def load_trained(path):
 
 		## Stackoverflow magic ##
 		xmin = x[np.argmin(vloss)]
-		ymin = vloss.min()
+		ymin = min(vloss)
 		text = f'Best model (loss: {ymin:.3f})'
 		bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
 		arrowprops=dict(arrowstyle="->",connectionstyle="angle,angleA=0,angleB=60")
