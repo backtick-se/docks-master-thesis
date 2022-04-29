3c8b8b2fc010d59e931bc91fbe1de3eef74ec2ad
Szymon Stypa <shabo.o@hotmail.com>
2022-04-29 19:04:25 +0200

    scheduled script

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from train import DiffTrainer
 
 if __name__ == '__main__':
-    config = {
+    base_config = {
         'rmo': True,
         'per_file': False,
         'distill': True,
@@ -11,8 +11,39 @@ if __name__ == '__main__':
         'split': 0.8,
         'seed': 42,
         'thaw': 0,
-        'lr': 1e-5,
+        'lr': 1e-3,
     }
-    trainer = DiffTrainer('distilbert-base-uncased',
-                          'temp.pickle', config=config)
-    trainer.train()
+
+    schedule = [{
+        'cp': 'distilbert-base-uncased',
+        'configs': [{
+            'thaw': 1
+        }]
+    }, {
+        'cp': 'huggingface/CodeBERTa-small-v1',
+        'configs': [{
+            'thaw': 1
+        }]
+    }, {
+        'cp': 'microsoft/codebert-base',
+        'configs': [{
+            'thaw': 0
+        }, {
+            'thaw': 1
+        }]
+    }, {
+        'cp': 'jeniya/BERTOverflow',
+        'configs': [{
+            'thaw': 0
+        }, {
+            'thaw': 1
+        }]
+    }]
+
+    for entry in schedule:
+        cp = entry['cp']
+
+        for config in entry['configs']:
+            cf = {**base_config, **config}
+            trainer = DiffTrainer(cp, 'scraped_diffs.pickle', config=cf)
+            trainer.train()
